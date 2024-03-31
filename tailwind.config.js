0ff2ec997934dab616013b058e6660621b10ef1e
natnael <natnaelmalike@gmail.com>
2024-03-31 23:49:29 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#FF8700',
+        secondary: '#d6d6d6'
+
+      },
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif']
+
+      },
+      backgroundImage:{
+        homeBg: 'url("https://assets.website-files.com/63d0c13bf294b9ad2ad7a1f0/63fc8f011df7042b653ee41c_hero-image-1-1.jpg")'
+      }
+    },
+  },
+  plugins: [],
+}
+
