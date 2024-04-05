effb4ed51989714b079898d06cdbd3b118541dd7
natnael <natnaelmalike@gmail.com>
2024-04-06 02:10:36 +0300

    making the mobile nav active

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
       },
       backgroundImage:{
         homeBg: 'url("https://assets.website-files.com/63d0c13bf294b9ad2ad7a1f0/63fc8f011df7042b653ee41c_hero-image-1-1.jpg")'
+      },
+      spacing:{
+        min_vh: 'min(10rem, 20vh) 4rem'
+      },
+      screens:{
+        sml: {'max': '639px'}
       }
     },
   },
