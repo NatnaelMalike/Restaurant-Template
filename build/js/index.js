9104a29c0750069fc4e5569de1fe074c00563855
natnael <natnaelmalike@gmail.com>
2024-04-09 23:11:54 +0300

    finishing the absic UI for the template

--- build/js/index.js
+++ build/js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+
     const foods = {
         breakfast: [
             {
@@ -217,30 +217,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const displayedFood = document.querySelector("#displayed-food");
 
     let index = 0;
-    let activeCatagory = 'breakfast';
-    let hasNext = index < foods[activeCatagory].length -1;
+    let activeCatagory = "breakfast";
+    let hasNext = index < foods[activeCatagory].length - 1;
     let hasPrev = index > 0;
 
-    const tabInit = document.querySelector('[aria-controls="breakfast"]')
-    tabInit.classList.add('active')
-    renderCategory('breakfast')
-    mobileRender('breakfast')
+    const tabInit = document.querySelector('[aria-controls="breakfast"]');
+    tabInit.classList.add("active");
+    renderCategory("breakfast");
+    mobileRender("breakfast");
+    
 
     const tabs = document.querySelectorAll('[role="tab"]');
     tabs.forEach((tab) => {
-        tab.addEventListener("click", function(){
+        tab.addEventListener("click", function () {
             const foodCategory = this.getAttribute("aria-controls");
-            activeCatagory = foodCategory
+            activeCatagory = foodCategory;
             tabs.forEach((tab) => {
-                tab.classList.remove('active');
-            })
-            tab.classList.add('active')
-            index = 0
+                tab.classList.remove("active");
+            });
+            tab.classList.add("active");
+            index = 0;
             renderCategory(foodCategory);
-            mobileRender(foodCategory)
+            mobileRender(foodCategory);
         });
     });
- 
+
     function mobileRender(foodCategory) {
         let menu = foods[foodCategory];
         let foodItem = menu[index];
@@ -250,25 +251,34 @@ document.addEventListener("DOMContentLoaded", function () {
                 <img
                     src=${foodItem.imgAddress}
                     alt="" />
-                <h2 class="text-white text-xl">
+                <h2 class="text-white text-xl it-name">
                     ${foodItem.title}
                 </h2>
                 <p class="text-secondary">
                     ${foodItem.ingredients}
                 </p>
                 <div class="flex justify-between border-t pt-4">
-                    <p class="text-white">${foodItem.price} ETB</p>
-                    <button>
+                <p class="text-white"> <span class="it-price">${foodItem.price}</span> ETB</p>
+                <button class="addToCart">
                         <i
                             class="fa-solid fa-cart-shopping text-white"></i>
                     </button>
                 </div>
             </article>
             `;
-            hasNext = index < foods[foodCategory].length -1;
-            hasPrev = index > 0;
+        hasNext = index < foods[foodCategory].length - 1;
+        hasPrev = index > 0;
+        const addCartBtn = displayedFood.querySelector('.addToCart')
+            addCartBtn.addEventListener('click', function(){
+                let itemPrice =
+                                this.parentElement.parentElement.querySelector(".it-price").textContent;
+                            let itemName =
+                                this.parentElement.parentElement.querySelector(".it-name").textContent;
+                            addItem(itemName, itemPrice);
+                            updateCart();
+            })
+        
     }
-
     function renderCategory(category) {
         const foodList = foods[category];
         const menuContainer = document.getElementById("food-container");
@@ -286,25 +296,33 @@ document.addEventListener("DOMContentLoaded", function () {
           <img
                         src=${food.imgAddress}
                         alt="" />
-                    <h2 class="text-white text-xl">
+                    <h2 class="text-white text-xl it-name">
                         ${food.title}
                     </h2>
                     <p class="text-secondary">
                         ${food.ingredients}
                     </p>
                     <div class="flex justify-between border-t pt-4">
-                        <p class="text-white">${food.price} ETB</p>
-                        <button>
+                        <p class="text-white"> <span class="it-price">${food.price}</span> ETB</p>
+                        <button class="addToCart">
                             <i
                                 class="fa-solid fa-cart-shopping text-white"></i>
                         </button>
                     </div>
           `;
             menuContainer.appendChild(foodItem);
+            let addCartBtn = foodItem.querySelector('.addToCart')
+            addCartBtn.addEventListener('click', function() {
+                let itemPrice =
+                                this.parentElement.parentElement.querySelector(".it-price").textContent;
+                            let itemName =
+                                this.parentElement.parentElement.querySelector(".it-name").textContent;
+                            addItem(itemName, itemPrice);
+                            updateCart();
+            })
         });
+       
     }
-
-
     nextBtn.addEventListener("click", handleNextClick);
     prevBtn.addEventListener("click", handlePrevClick);
 
@@ -314,7 +332,6 @@ document.addEventListener("DOMContentLoaded", function () {
             mobileRender(activeCatagory);
         }
     }
-
     function handlePrevClick() {
         if (hasPrev) {
             index--;
@@ -322,4 +339,73 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-})
+    const cartPage = document.querySelector("#cart");
+    let cartPageItems = document.querySelector(".selected-foods-only");
+    let cartItems = [];
+    let totalQuantityEle = document.querySelector(".total-quantity-number");
+    let totalPriceEle = document.querySelector(".total-price-number");
+    let cartCloseBtn = document.querySelector("#cart-close");
+    let cartBtn = document.querySelector("#cartButton");
+
+    cartBtn.addEventListener("click", () => {
+        cartPage.classList.remove("hidden");
+    });
+    cartCloseBtn.addEventListener("click", () => {
+        cartPage.classList.add("hidden");
+    });
+
+
+    function addItem(itemName, itemPrice) {
+        const existingItem = cartItems.find(
+            (item) => item.itemName === itemName
+        );
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            cartItems.push({ itemName, itemPrice, quantity: 1 });
+        }
+    }
+
+    function removeItem(index) {
+        cartItems.splice(index, 1);
+        updateCart();
+    }
+
+    function addOne(index) {
+        cartItems[index].quantity++;
+        updateCart();
+    }
+    function minusOne(index) {
+        if (cartItems[index].quantity === 1) {
+            removeItem(index);
+        }
+        cartItems[index].quantity--;
+        updateCart();
+    }
+    function updateCart() {
+        let totalPrice = 0;
+        let totalQuantity = 0;
+        cartPageItems.innerHTML = "";
+
+        cartItems.forEach((item, index) => {
+            cartPageItems.innerHTML += `<div class="selected-foods flex justify-between items-center border-b border-white py-4 px-2">
+            <h4 class="item-title text-white">${item.itemName}</h4>
+            <p class="item-price text-white">${item.itemPrice}</p>
+            <div class="item-quantity">
+            <button class="minus btn-popUp text-secondary" onclick='minusOne(${index})'><i class="fa-solid fa-minus"></i></button>
+            <span class="item-quantity-display text-white">${item.quantity}</span>
+            <button class="plus btn-popUp text-secondary" onclick='addOne(${index})'><i class="fa-solid fa-plus"></i></button>
+            </div>
+            <div class="delete-item text-red-500" onclick='removeItem(${index})'><i class="fa-solid fa-trash"></i></div>
+        </div>`;
+
+            totalPrice += parseInt(item.itemPrice) * item.quantity;
+            totalQuantity += item.quantity;
+        });
+        totalQuantityEle.textContent = totalQuantity;
+        totalPriceEle.textContent = totalPrice;
+    }
+
+    
+   
+
